10472d086957bb8ebdbff8dc25b925b55bbc5467
Mikhail Aksenov <aksenov364@gmail.com>
2014-10-10 21:19:07 +0600

    Single-threaded version works

--- Program.cs
+++ Program.cs
@@ -4,33 +4,96 @@ using System.Linq;
 using System.Drawing;
 using System.Drawing.Imaging;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace median
 {
     class Program
     {
-        static void Median(int pos, ref byte[] img) 
+        static readonly int MaskSize = 9;
+        static readonly int Med = (int) Math.Round(MaskSize / 2.0);
+        static readonly int Diff = 3;
+        
+        static void CalcValue(int row, int col, int shift, int width, ref byte[] source, ref byte[] target)
         {
+            byte[] mask = new byte[MaskSize];
+            mask[0] = source[(row - 1) * width + col - Diff + shift];
+            mask[1] = source[(row - 1) * width + col + shift];
+            mask[2] = source[(row - 1) * width + col + Diff + shift];
 
+            mask[3] = source[(row * width) + col - Diff + shift];
+            mask[4] = source[(row * width) + col + shift];
+            mask[5] = source[(row * width) + col + Diff + shift];
+
+            mask[6] = source[(row + 1) * width + col - Diff + shift];
+            mask[7] = source[(row + 1) * width + col + shift];
+            mask[8] = source[(row + 1) * width + col + Diff + shift];
+
+            Array.Sort(mask);
+
+            target[(row * width) + col + shift] = mask[Med];
         }
 
-        static void Filter(string inputPath, string outputPath)
+
+        /// Single-Thread filter
+        static void STFilter(string inputPath, string outputPath)
         {
             var img = new Bitmap(inputPath);
             var rect = new Rectangle(0, 0, img.Width, img.Height);
-            BitmapData bitmapData = img.LockBits(rect, ImageLockMode.ReadWrite, img.PixelFormat);
-            IntPtr ptr = bitmapData.Scan0;
-            int length = Math.Abs(bitmapData.Stride) * img.Height;
-            byte[] data = new byte[length];
+            var bitmapData = img.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            var ptr = bitmapData.Scan0;
+            var length = Math.Abs(bitmapData.Stride) * img.Height;
+            var sourceBytes = new byte[length];
+            var targetBytes = new byte[length];
+            var width = img.Width * 3;
 
-            System.Runtime.InteropServices.Marshal.Copy(ptr, data, 0, length);
+            System.Runtime.InteropServices.Marshal.Copy(ptr, sourceBytes, 0, length);
 
-            for (int i = 2; i < data.Length; i += 3)
+            for (int i = 1; i < img.Height - 1; i++)
             {
-                data[i] = 255;
+                for (int j = 1; j < img.Width - 1; j++)
+                {
+                    CalcValue(i, j * 3, 0, width, ref sourceBytes, ref targetBytes);
+                    CalcValue(i, j * 3, 1, width, ref sourceBytes, ref targetBytes);
+                    CalcValue(i, j * 3, 2, width, ref sourceBytes, ref targetBytes);
+                }
             }
 
-            System.Runtime.InteropServices.Marshal.Copy(data, 0, ptr, length);
+            System.Runtime.InteropServices.Marshal.Copy(targetBytes, 0, ptr, length);
+
+            img.UnlockBits(bitmapData);
+            img.Save(outputPath, ImageFormat.Bmp);
+        }
+
+        static void InvokeFilter(ref byte[] source, ref byte[] target, int step, int w, int h)
+        { 
+        }
+
+        static void MTFilter(string inputPath, string outputPath)
+        {
+            var img = new Bitmap(inputPath);
+            var rect = new Rectangle(0, 0, img.Width, img.Height);
+            var bitmapData = img.LockBits(rect, ImageLockMode.ReadWrite, PixelFormat.Format24bppRgb);
+            var ptr = bitmapData.Scan0;
+            var length = Math.Abs(bitmapData.Stride) * img.Height;
+            var sourceBytes = new byte[length];
+            var targetBytes = new byte[length];
+            var width = img.Width * 3;
+
+            System.Runtime.InteropServices.Marshal.Copy(ptr, sourceBytes, 0, length);
+
+            for (int i = 1; i < img.Height - 1; i++)
+            {
+                for (int j = 1; j < img.Width - 1; j++)
+                {
+                    CalcValue(i, j * 3, 0, width, ref sourceBytes, ref targetBytes);
+                    CalcValue(i, j * 3, 1, width, ref sourceBytes, ref targetBytes);
+                    CalcValue(i, j * 3, 2, width, ref sourceBytes, ref targetBytes);
+                }
+            }
+
+            System.Runtime.InteropServices.Marshal.Copy(targetBytes, 0, ptr, length);
 
             img.UnlockBits(bitmapData);
             img.Save(outputPath);
@@ -39,8 +102,9 @@ namespace median
 
         static void Main(string[] args)
         {
-            Filter(args[0], args[1]);
-            //Filter(@"C:\dev\metro\median\maxresdefault.jpg", "some.jpg");
+            var time = DateTime.Now.Millisecond;
+            STFilter(args[0], args[1]);
+            Console.WriteLine("Single-Threaded taken {0} ms", DateTime.Now.Millisecond - time);
         }
     }
 }
